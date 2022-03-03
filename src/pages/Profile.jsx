c880d9e30d5d8db9a23f2a21da5122b94472a035
ermias kidane <ermiaskidanegebre@gmail.com>
2022-03-03 00:31:11 +0000

    first commit

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import { getAuth } from 'firebase/auth'
+
+function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+
+  useEffect(() => {
+    setUser(auth.currentUser)
+    // console.log(auth.currentUser)
+  }, [])
+  return user ? <h1>{user.displayName}</h1> : 'No Logged In'
+}
+
+export default Profile

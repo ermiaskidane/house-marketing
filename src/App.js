c99883f25f618f4165345b2308a07948fd08b6ea
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-03-05 21:44:03 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
-import Offer from './pages/Offer'
+import Offer from './pages/Offers'
+import Category from './pages/Category'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import CreateListing from './pages/CreateListing'
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offer />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
@@ -24,6 +27,7 @@ function App() {
           <Route path='/Sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>

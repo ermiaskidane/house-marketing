104407d5426f5773cab9a858666ebbd3b49eaf8b
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-03-12 17:46:36 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import CreateListing from './pages/CreateListing'
+import EditListing from './pages/EditListing'
 import Listing from './pages/Listing'
 import Contact from './pages/Contact'
 

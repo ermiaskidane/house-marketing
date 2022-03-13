e8cfa2029dc060f15d7ba2a1a8e53235bbbf6567
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-03-13 12:57:32 +0000

    Update Listing.jsx

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 // import { Helmet } from 'react-helmet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// import 'swiper/swiper-bundle.css'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
-// SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 function Listing() {
   const [listing, setListing] = useState(null)
@@ -39,7 +39,7 @@ function Listing() {
     return <Spinner />
   }
 
-  console.log(listing)
+    
   return (
     <main>
       <Swiper slidesPerView={1} pagination={{ clickable: true }}>
